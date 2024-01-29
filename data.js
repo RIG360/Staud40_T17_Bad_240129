154721bde05505b29bf74971c207ff09908ea35f
RIG360 <office@rimpfl.at>
2024-01-29 17:31:44 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2dg-t17-wz",
+      "name": "2DG, T17, WZ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.09100166312560631,
+        "pitch": -0.022263893195137996,
+        "fov": 1.6820569460893378
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.015435101310979249,
+          "pitch": 0.47662299677945796,
+          "rotation": 0,
+          "target": "1-2dg-t17-bad-vr"
+        },
+        {
+          "yaw": -0.01515358352089713,
+          "pitch": 0.3373728245951746,
+          "rotation": 0,
+          "target": "2-2dg-t17-wc"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2dg-t17-bad-vr",
+      "name": "2DG, T17, Bad-VR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07445888349021956,
+          "pitch": 0.0020105376870453995,
+          "rotation": 0,
+          "target": "2-2dg-t17-wc"
+        },
+        {
+          "yaw": -1.440489819096296,
+          "pitch": 0.00480585159026603,
+          "rotation": 0,
+          "target": "3-2dg-t17-bad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2dg-t17-wc",
+      "name": "2DG, T17, WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1386848724830045,
+          "pitch": 0.8011186174786769,
+          "rotation": 0,
+          "target": "1-2dg-t17-bad-vr"
+        },
+        {
+          "yaw": 3.090211770987886,
+          "pitch": 0.154223328936272,
+          "rotation": 0,
+          "target": "0-2dg-t17-wz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2dg-t17-bad",
+      "name": "2DG, T17, Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.847190883522317,
+          "pitch": 0.7045243836740731,
+          "rotation": 0,
+          "target": "1-2dg-t17-bad-vr"
+        },
+        {
+          "yaw": 2.1493657655910807,
+          "pitch": 0.07249517399449701,
+          "rotation": 0,
+          "target": "0-2dg-t17-wz"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Staud40, T7, Bad",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
